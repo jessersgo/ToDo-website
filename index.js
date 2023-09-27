016666708f82616bcc56918ae165993b152dc462
Jessica <jessicaguevara193@gmail.com>
2023-09-27 01:00:12 -0400

    testing

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const input = getElementById("task");
+const tasksList = getElementById("list"); 
+
+addTask = () =>{
+    //first check it has something added/ not an empty string
+    //since js must use === for strict equality
+    if(input.value === ''){
+        alert("Please type in a task to add :)!")
+    } else{
+        let
+    }
+
+}
